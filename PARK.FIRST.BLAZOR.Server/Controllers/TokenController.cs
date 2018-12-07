2fce09ad47f0037d8c19911a589570c2a2f8c3da
JongSoo Park <newtein80@gmail.com>
2018-12-08 04:02:32 +0900

    초기 커밋.

---
+++ PARK.FIRST.BLAZOR.Server/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PARK.FIRST.BLAZOR.Server.Interface;
+using PARK.FIRST.BLAZOR.Shared;
+
+namespace PARK.FIRST.BLAZOR.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IJwtTokenService _tokenService;
+
+        public TokenController(IJwtTokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        public IActionResult GenerateToken([FromBody] TokenViewModel vm)
+        {
+            var token = _tokenService.BuildToken(vm.Email);
+
+            return Ok(new { token });
+        }
+    }
+}
---
+++ PARK.FIRST.BLAZOR.Server/Interface/IJwtTokenService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARK.FIRST.BLAZOR.Server.Interface
+{
+    public interface IJwtTokenService
+    {
+        string BuildToken(string email);
+    }
+}
